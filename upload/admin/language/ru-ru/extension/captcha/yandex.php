671f7722e821d010de6d498a5c49ac9fb7becb2d
19th <19th19th@gmail.com>
2026-02-26 10:10:05 +0600

    Яндекс капча

---
+++ upload/admin/language/ru-ru/extension/captcha/yandex.php
@@ -0,0 +1,21 @@
+<?php
+// *	@source		See SOURCE.txt for source and other copyright.
+// *	@license	GNU General Public License version 3; see LICENSE.txt
+
+$_['heading_title']    = 'Яндекс CAPTCHA';
+
+// Text
+$_['text_captcha']     = 'Капча';
+$_['text_success']	   = 'Настройки успешно изменены!';
+$_['text_edit']        = 'Настройки Яндекс CAPTCHA';
+$_['text_signup']      = 'Перейдите <a href="https://console.yandex.cloud/services/smartcaptcha" target="_blank"><u>Яндекс CAPTCHA</u></a> и зарегистрируйте свой магазин.';
+
+// Entry
+$_['entry_key']        = 'Ключ клиента';
+$_['entry_secret']     = 'Ключ сервера';
+$_['entry_status']     = 'Статус';
+
+// Error
+$_['error_permission'] = 'У вас недостаточно прав для внесения изменений!';
+$_['error_key']	       = 'Необходим ключ клиента!';
+$_['error_secret']	   = 'Необходим ключ серера!';
