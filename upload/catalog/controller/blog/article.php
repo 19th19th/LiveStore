c3fff413b0d6a218a07c09d6b3209b8f86e05bb6
19th19th <19th19th@gmail.com>
2026-04-09 12:53:54 +0000

    Исправление блога, админки, перевода, верстки

--- upload/catalog/controller/blog/article.php
+++ upload/catalog/controller/blog/article.php
@@ -126,10 +126,6 @@ class ControllerBlogArticle extends Controller {
 			$this->document->addLink($this->url->link('blog/article', 'article_id=' . $this->request->get['article_id']), 'canonical');
 			$this->document->addScript('catalog/view/javascript/jquery/magnific/jquery.magnific-popup.min.js');
 			$this->document->addStyle('catalog/view/javascript/jquery/magnific/magnific-popup.css');
-			$this->document->addScript('catalog/view/javascript/jquery/datetimepicker/moment/moment.min.js');
-			$this->document->addScript('catalog/view/javascript/jquery/datetimepicker/moment/moment-with-locales.min.js');
-			$this->document->addScript('catalog/view/javascript/jquery/datetimepicker/bootstrap-datetimepicker.min.js');
-			$this->document->addStyle('catalog/view/javascript/jquery/datetimepicker/bootstrap-datetimepicker.min.css');
 
 			if ($article_info['meta_h1']) {	
 				$data['heading_title'] = $article_info['meta_h1'];
