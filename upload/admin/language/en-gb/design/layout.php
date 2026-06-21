167d21b7d683dba8df0fd3f28851675b35e9c1b1
Artem Serbulenko <artserbulenko@gmail.com>
2026-06-21 19:46:29 +0300

    error text

--- upload/admin/language/en-gb/design/layout.php
+++ upload/admin/language/en-gb/design/layout.php
@@ -39,3 +39,5 @@ $_['error_product']       = 'Warning: This layout cannot be deleted as it is cur
 $_['error_category']      = 'Warning: This layout cannot be deleted as it is currently assigned to %s categories!';
 $_['error_manufacturer']  = 'Warning: This layout cannot be deleted as it is currently assigned to %s manufacturers!';
 $_['error_information']   = 'Warning: This layout cannot be deleted as it is currently assigned to %s information pages!';
+$_['error_blog_article']  = 'Warning: This layout cannot be deleted as it is currently assigned to %s blog articles!';
+$_['error_blog_category'] = 'Warning: This layout cannot be deleted as it is currently assigned to %s blog categories!';
